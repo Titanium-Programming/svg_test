f70abea0d2976afbba7f22b6aff5f327cdec28f1
₸itanium ₱rogramming <70477057+Titanium-Programming@users.noreply.github.com>
2020-11-27 17:24:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 console.clear();
-function svgEl(name, attr, to) {
-	var el = document.createElementNS("http://www.w3.org/2000/svg", name);
-	for(var i = 0; i < Object.keys(attr).length; i ++) {
-		el.setAttribute(Object.keys(attr)[i], attr[Object.keys(attr)[i]]);
+{
+	function svgEl(name, attr, to) {
+		var el = document.createElementNS("http://www.w3.org/2000/svg", name);
+		for(var i = 0; i < Object.keys(attr).length; i ++) {
+			el.setAttribute(Object.keys(attr)[i], attr[Object.keys(attr)[i]]);
+		}
+		to.appendChild(el);
+		return el;
 	}
-	to.appendChild(el);
-	return el;
-}
-function changeAttr(what, attr) {
-	for(var i = 0; i < Object.keys(attr).length; i ++) {
-		el.setAttribute(Object.keys(attr)[i], attr[Object.keys(attr)[i]]);
+	function changeAttr(what, attr) {
+		for(var i = 0; i < Object.keys(attr).length; i ++) {
+			el.setAttribute(Object.keys(attr)[i], attr[Object.keys(attr)[i]]);
+		}
 	}
-}
-function hide(from, what) {
-	from.removeChild(what);
-}
-function show(from, what) {
-	from.appendChild(what);
-}
-var svg = document.getElementById("SVG"),
-	rect = svgEl("rect", {
-		x: 0,
-		y: 0,
-		width: 60,
-		height: 211
-	}, svg);
+	function hide(from, what) {
+		from.removeChild(what);
+	}
+	function show(from, what) {
+		from.appendChild(what);
+	}
+} // makes life easier
+(function() {
+	"use strict";
+	let svg = document.getElementById("SVG");
+	
+})(); // the main code
